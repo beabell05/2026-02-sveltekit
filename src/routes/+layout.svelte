fcaab161d875b7773237de44f3235597c2ec7de0
beabell05 <269154039+beabell05@users.noreply.github.com>
2026-04-26 11:08:49 +0200

    add custom fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,15 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+
+	<!-- Satoshi font -->
+	<link rel="preconnect" href="https://api.fontshare.com/" />
+	<link href="https://api.fontshare.com/v2/css?f[]=satoshi@1,2&display=swap" rel="stylesheet">
+
+	<!-- Inter font -->
+	<link rel="preconnect" href="https://fonts.bunny.net">
+    <link rel="preconnect" href="https://fonts.bunny.net" crossorigin>
+    <link href="https://fonts.bunny.net/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
 </svelte:head>
 
 {@render children()}
